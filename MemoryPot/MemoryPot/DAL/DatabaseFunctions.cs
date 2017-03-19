cc42bdc1eb50c9b9f036bd0a0057ac5a5c669774
feni000 <phoenixa27@gmail.com>
2017-03-19 22:52:40 +0100

    end of first day work, sqlite password hashing auth not working

---
+++ MemoryPot/MemoryPot/DAL/DatabaseFunctions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MemoryPot.DAL
+{
+    class DatabaseFunctions
+    {
+        public static void UnhashPassword(string hashedPassword, string hashedPasswordFromDatabase)
+        {
+            byte[] hashBytes = Convert.FromBase64String(hashedPasswordFromDatabase);
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+            var pbkdf2 = new Rfc2898DeriveBytes(hashedPassword, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            for (int i = 0; i < 20; i++)
+                if (hashBytes[i + 16] != hash[i])
+                    throw new UnauthorizedAccessException();
+        }
+        public static Boolean LoginDB(string dbName, string dbUserName, string dbPassword)
+        {
+            try
+            {
+                string startupPath = Environment.CurrentDirectory;
+                string dataBasePath = startupPath + "\\..\\..\\DB\\" + dbName;
+                SQLiteConnection dbConnString;
+                dbConnString = new SQLiteConnection("Data Source =" + dataBasePath + ";Version=3;");
+                dbConnString.Open();
+                using (SQLiteCommand checkConnection = new SQLiteCommand("SELECT UserPasswordHash FROM Users where UserName = \"" + dbUserName + "\";", dbConnString))
+                {
+                    using (var reader = checkConnection.ExecuteReader())
+                    {
+                        var listOfUsers = new List<string>();
+                        while (reader.Read())
+                            listOfUsers.Add(reader.GetString(0));
+                        foreach(var item in listOfUsers)
+                        {
+                            UnhashPassword(dbPassword, item);
+                        }
+                    }
+                }
+                dbConnString.Close();
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Nie określony błąd, skontaktuj się z administratorem.", "DB Info");
+                return false;
+            }
+        }
+    }
+}
---
+++ MemoryPot/MemoryPot/View/LoginWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using MemoryPot.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MemoryPot.View
+{
+    /// <summary>
+    /// Interaction logic for LoginWindodw.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string HashPassword(string password)
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+            var pbkdf2 = new Rfc2898DeriveBytes(PasswordTextbox.Password, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+            string savedPasswordHash = Convert.ToBase64String(hashBytes);
+            return savedPasswordHash;
+        }
+
+        private void LoginDB_Click(object sender, RoutedEventArgs e)
+        {
+            string hashedPassword = HashPassword(PasswordTextbox.Password);
+            if (DatabaseFunctions.LoginDB("DBMemoryPot.db", UserNameTextBox.Text, hashedPassword) == true)
+            {
+                //UserWindow userActiveWindow = new UserWindow();
+                //User loggedUser = new User(1, UserNameTextBox.Text, PasswordTextbox.Password, 1);
+                //userActiveWindow.Show();
+                //this.Close();
+            }
+        }
+    }
+}
